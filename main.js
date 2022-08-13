6c4ce1afc4ad24233bde41a759ddcdd0ee674878
viktaur <victorgabal@gmail.com>
2022-08-13 10:08:41 +0200

    create dummy car

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1),100,30,50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const traffic = [
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+];
 
 animate();
 
 function animate() {
+    // update every car on traffic
+    for (let i=0; i < traffic.length; i++) {
+        traffic[i].update(road.borders);
+    }
+
+    // update the main car
     car.update(road.borders);
 
     canvas.height = window.innerHeight;
@@ -16,6 +25,13 @@ function animate() {
     ctx.translate(0, -car.y + canvas.height * 0.7);
 
     road.draw(ctx);
+
+    // draw every car on traffic
+    for(let i=0; i < traffic.length; i++) {
+        traffic[i].draw(ctx);
+    }
+
+    // draw the main car
     car.draw(ctx);
 
     ctx.restore();
